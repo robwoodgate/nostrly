a338f587d821863fc63c5fa003cfc353b3f63b07
Rob Woodgate <robwoodgate@mac.com>
2025-01-26 20:13:49 +0000

    BETA: Building register page

--- lib/class-nostrly-admin.php
+++ lib/class-nostrly-admin.php
@@ -42,9 +42,9 @@ class NostrlyAdmin
     {
         global $menu, $pagenow;
 
-        if (!self::is_needed()) {
-            return;
-        }
+        // if (!self::is_needed()) {
+        //     return;
+        // }
 
         // Redirect contributors to profile page
         if (is_admin() && !defined('DOING_AJAX') && !current_user_can('edit_posts') && current_user_can('subscriber') && !strpos($_SERVER['REQUEST_URI'], 'profile.php')) {
@@ -65,14 +65,14 @@ class NostrlyAdmin
         remove_action('admin_notices', 'update_nag', 3);
 
         // * Remove standard Dashboard Widgets
-        remove_meta_box('dashboard_activity', 'dashboard', 'normal'); // since 3.8
-        remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
-        remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
+        // remove_meta_box('dashboard_activity', 'dashboard', 'normal'); // since 3.8
+        // remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
+        // remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
         remove_meta_box('dashboard_primary', 'dashboard', 'normal');
-        remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
-        remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
-        remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
-        remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
+        // remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+        // remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
+        // remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+        // remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
         remove_meta_box('dashboard_secondary', 'dashboard', 'normal');
         remove_meta_box('dashboard_browser_nag', 'dashboard', 'normal');
         remove_meta_box('dashboard_php_nag', 'dashboard', 'normal');
