f7329cb8734034c2e841d23e4efe6084324f3ae6
Rob Woodgate <robwoodgate@mac.com>
2026-09-03 17:34:45 +0100

    chore(deps): bump cashu-ts to 5.0.0-experimental.40ac94342

--- nostrly.php
+++ nostrly.php
@@ -26,7 +26,7 @@ define('NOSTRLY_PATH', plugin_dir_path(__FILE__));
 define('NOSTRLY_URL', plugin_dir_url(__FILE__));
 define('NOSTRLY_SLUG', plugin_basename(__DIR__));
 define('NOSTRLY_FILE', plugin_basename(__FILE__));
-define('NOSTRLY_VERSION', '15.17.0-5.0.0-experimental.e3dcfa01f');
+define('NOSTRLY_VERSION', '15.17.0-5.0.0-experimental.40ac94342');
 
 // * Instantiate main plugin
 require_once NOSTRLY_PATH.'lib/class-nostrly.php';
